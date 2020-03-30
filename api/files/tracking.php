1c5317fbed4a2574030d5566c7971c22ff44f85b
konark kapil <konarkkapil@rocketmail.com>
2020-03-30 14:52:42 +0530

    added name instead of userid in file tracking

--- api/files/tracking.php
+++ api/files/tracking.php
@@ -16,12 +16,23 @@
     //fetching all records from db table
     if(mysqli_num_rows($query) > 0){
         while($res = mysqli_fetch_assoc($query)){
+            
+            $sql_from_name = "select name from users where user_id=".$res['from_id'];
+            $query_from_name = mysqli_query($conn,$sql_from_name);
+            $get_from_name = mysqli_fetch_assoc($query_from_name);
+            $from_name = $get_from_name['name'];
+
+            $sql_to_name = "select name from users where user_id=".$res['to_id'];
+            $query_to_name = mysqli_query($conn,$sql_to_name);
+            $get_to_name = mysqli_fetch_assoc($query_to_name);
+            $to_name = $get_to_name['name'];
+
             $filedata[] = array(
                 "serial" => $res['serial'],
                 "trackingid" => $res['tracking_id'],
                 "file_number" => $res['file_number'],
-                "from" => $res['from_id'],
-                "to" => $res['to_id'],
+                "from" => $from_name,
+                "to" => $to_name,
                 "timestamp"=>$res['transfer_timestamp']
             );
         }
